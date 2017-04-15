bc3e11231e696ce32b6ebcf368d0c616c976e5d9
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-15 20:25:17 +0000

    Add tests for predictionMaker

--- spec/predictionMakerSpec.js
+++ spec/predictionMakerSpec.js
@@ -17,21 +17,47 @@ describe("prediction Maker", function() {
       expect(companyProperty.company).toBe("Apple");
     });
     it("takes a company with multiple words and returns the company property on the prediction object ", function() {
-      let companyProperty = predictionMaker("My Mom's Dirty Socks");
-      expect(companyProperty.company).toBe("My Mom's Dirty Socks");
+      let predictionObj = predictionMaker("Take 2 Interactive");
+      expect(predictionObj.company).toBe("Take 2 Interactive");
     });
-    it("returns a formatter prediction as a string", function() {
-      let prediction = `Prediction for today ${time}: Apple stocks are going up.`;
-      let companyProperty = predictionMaker("My Mom's Dirty Socks");
-      expect(companyProperty.company).toBe("My Mom's Dirty Socks");
+    it("returns on object with prediction property", function() {
+      let predictionObj = predictionMaker("Take 2 Interactive");
+      expect(predictionObj.prediction).toBeDefined();
     });
-    xit("returns up or down at the end of the prediction", function() {
-      expect(/.+up\.$|down\.$/.test(predictionMaker())).toEqual(true);
+    it("returns on object with prediction property of up or down", function() {
+      let predictionObj = predictionMaker("Take 2 Interactive");
+      expect(/^up$|^down$/.test(predictionObj.prediction)).toBe(true);
     });
+    it("returns on object with time property", function() {
+      let predictionObj = predictionMaker("Take 2 Interactive");
+      expect(predictionObj.time).toBeDefined();
+    });
+    it("returns on object with time of prediction creation", function() {
+      let predictionObj = predictionMaker("Take 2 Interactive");
+      expect(predictionObj.time).toBe(time);
+    });
+    it("returns on object with prediction time as a string", function() {
+      let predictionObj = predictionMaker("Take 2 Interactive");
+      expect(typeof predictionObj.time).toBe("string");
+    });
+
   });
+
   describe("a call to callAlgorithim", function() {
     it("check if method returns up or down", function() {
-      expect(/up|down/.test(callAlgorithim())).toEqual(true);
+      expect(/^up$|^down$/.test(callAlgorithim())).toEqual(true);
     });
   });
+
 });
+
+// xdescribe("prediction formatter", function() {
+//   xit("returns a formatter prediction as a string", function() {
+//     let prediction = `Prediction for today ${time}: Apple stocks are going up.`;
+//     let predictionObj = predictionMaker("Apple");
+//     expect(predictionObj.prediction).toBe(prediction);
+//   });
+//   xit("returns up or down at the end of the prediction", function() {
+//     expect(/.+up\.$|down\.$/.test(predictionMaker())).toEqual(true);
+//   });
+// });
