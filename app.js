450ab4fe9265e41f51eadc218baf6606d812d6b1
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-15 23:26:57 +0000

    Add timer for recalling the function

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ const express = require("express");
 const TwitterWrapper = require("./lib/twitter_wrapper");
 const twit = new TwitterWrapper(require("./configtwit.json"));
 
-twit.sendPrediction("It's gonna rain!").then(response => {
-    if (typeof response === "string") {
-        console.error(response);
-    }
-    else {
-        console.log(`Response from twitter API ${response.data}`);
-    }
-});
+////************* Prediction Maker Module ********////
+const PredictionMaker = require('./lib/predictionMaker');
+const predictionMaker = new PredictionMaker();
+//////////*******Prediction Formatter*********//////////////////////
+const predictionFormatter = require('./lib/predictionFormatter');
+///////////////*****************//////////////////////
+
+//get a prediction
+async function tweetPrediction() {
+    let predictionObj = await predictionMaker.getPrediction('Apple')
+    let predictionString = predictionFormatter(predictionObj);
+    twit.sendPrediction(predictionString).then(response => {
+        if (typeof response === "string") {
+            console.error(response);
+        }
+        else {
+            console.log(`Response from twitter API ${response.data}`);
+        }
+    });
+}
+
+//format it
+let i = 0;
+while ()
--- spec/predictionFormatterSpec.js
+++ spec/predictionFormatterSpec.js
@@ -1,13 +1,42 @@
 const predictionFormatter = require("../lib/predictionFormatter");
 let predictionObj;
+let expectedString;
 let time = new Date(Date.now()).toDateString();
 describe("predictionFormatter Module", function() {
-  beforeEach(function() {
+  it("returns down for 0 prediction", function() {
     predictionObj = {
       time: time,
       prediction: 0,
       company: "Apple"
     };
+    expectedString = `Prediction for today ${time}: Apple stocks are going down.`;
+    expect(predictionFormatter(predictionObj)).toBe(expectedString);
   });
-  it();
+  it("returns up for 1 prediction", function() {
+    predictionObj = {
+      time: time,
+      prediction: 1,
+      company: "Apple"
+    };
+    expectedString = `Prediction for today ${time}: Apple stocks are going up.`;
+    expect(predictionFormatter(predictionObj)).toBe(expectedString);
+  });
+  it("returns up for 1 prediction and different name", function() {
+    predictionObj = {
+      time: time,
+      prediction: 1,
+      company: "Apple Pies"
+    };
+    expectedString = `Prediction for today ${time}: Apple Pies stocks are going up.`;
+    expect(predictionFormatter(predictionObj)).toBe(expectedString);
+  });
+  it("returns up for 0 prediction and different name", function() {
+    predictionObj = {
+      time: time,
+      prediction: 0,
+      company: "Apple Pies"
+    };
+    expectedString = `Prediction for today ${time}: Apple Pies stocks are going down.`;
+    expect(predictionFormatter(predictionObj)).toBe(expectedString);
+  });
 });
