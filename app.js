06ba503b33f6c216080bdebf6d85800b0eea7f35
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-15 17:45:53 +0000

    Add the wrapper to app.js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require("express");
-const fetch = require("isomorphic-fetch");
+//const fetch = require("isomorphic-fetch");
+
+const TwitterWrapper = require('./lib/twitter_wrapper');
+const twit = new TwitterWrapper(require('./configtwit.json'));
+
+
+twit.sendPrediction("It's gonna rain!").then(response => {
+    if (typeof response === 'string') {
+        console.error(response);
+    }
+    else {
+        console.log(`Response from twitter API ${ response.data } and ${ response.response }`);
+    }
+
+});
--- spec/twitter_wrapperSpec.js
+++ spec/twitter_wrapperSpec.js
@@ -3,21 +3,28 @@
 const TwitterWrapper = require("../lib/twitter_wrapper");
 
 describe(".sendPrediction method", function() {
-  let tweet;
+    let tweet;
 
-  beforeEach(function() {
-    tweet = new TwitterWrapper();
-  });
+    let fakeWrapperConfig = {
+        consumer_key: 'fake',
+        consumer_secret: 'config',
+        access_token: 'file',
+        access_token_secret: 'test',
+        timeout_ms: 60, // optional HTTP request timeout to apply to all requests.
+    };
+    beforeEach(function() {
+        tweet = new TwitterWrapper(fakeWrapperConfig);
+    });
 
-  it("return an a error if no string is passed", function() {
-    expect(tweet.checkLength()).toEqual(false);
-  });
-  it("return error if string length is greater than 140", function() {
-    let longStr = "fffasdkjfhlaksdfnalksjdfh;asdfjhlkjahsdkfjhalksjdfhlakjshdflkajshdflkjashdflkjasdfhlkajsdhflkjashdflkajshdflkjashdflkjahsdlfkjhasdlfkjhaslkdjfhalskjdfhalksjdfhlakjsdfhakjlsdhfkajsdhfkjadsfhalksdhjfasfl;kasjflaksdjf;liekjasdkl;fjakls;fdjakl;sdfjal;skdfjal;skdfjal;ksjdfal;ksdfjal;ksdfjal;skdfjal;ksdjfals;kj";
-    expect(tweet.checkLength(longStr)).toEqual(false);
-  });
-  it("expect .checkLength when called with str less than 140 chars and greater than zero to respond with 'success'", function() {
-    let goodStr = "Hello";
-    expect(tweet.checkLength(goodStr)).toEqual(true);
-  });
+    it("return an a error if no string is passed", function() {
+        expect(tweet.checkLength()).toEqual(false);
+    });
+    it("return error if string length is greater than 140", function() {
+        let longStr = "fffasdkjfhlaksdfnalksjdfh;asdfjhlkjahsdkfjhalksjdfhlakjshdflkajshdflkjashdflkjasdfhlkajsdhflkjashdflkajshdflkjashdflkjahsdlfkjhasdlfkjhaslkdjfhalskjdfhalksjdfhlakjsdfhakjlsdhfkajsdhfkjadsfhalksdhjfasfl;kasjflaksdjf;liekjasdkl;fjakls;fdjakl;sdfjal;skdfjal;skdfjal;ksjdfal;ksdfjal;ksdfjal;skdfjal;ksdjfals;kj";
+        expect(tweet.checkLength(longStr)).toEqual(false);
+    });
+    it("expect .checkLength when called with str less than 140 chars and greater than zero to respond with 'success'", function() {
+        let goodStr = "Hello";
+        expect(tweet.checkLength(goodStr)).toEqual(true);
+    });
 });
