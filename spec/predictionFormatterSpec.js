6674509180f4d358bd047c2deb681d481e8a9f69
markmarkyesyes <markhahn545@gmail.com>
2017-04-15 17:31:29 -0500

    finished testing predictionMaker module, changed tests to accept prediction property as 0 and 1 instead of up and down, began writing spec for prediction formatter

---
+++ spec/predictionFormatterSpec.js
@@ -0,0 +1,13 @@
+const predictionFormatter = require("../lib/predictionFormatter");
+let predictionObj;
+let time = new Date(Date.now()).toDateString();
+describe("predictionFormatter Module", function() {
+  beforeEach(function() {
+    predictionObj = {
+      time: time,
+      prediction: 0,
+      company: "Apple"
+    };
+  });
+  it();
+});
--- spec/predictionMakerSpec.js
+++ spec/predictionMakerSpec.js
@@ -1,4 +1,4 @@
-const PredictionMaker = require('../lib/predictionMaker');
+const PredictionMaker = require("../lib/predictionMaker");
 console.log(typeof PredictionMaker);
 let time;
 let predictionMaker;
@@ -8,51 +8,67 @@ describe("prediction Maker", function() {
       time = new Date(Date.now()).toDateString();
       predictionMaker = new PredictionMaker();
     });
-    it("check if method returns a prediction object", function() {
-      expect(typeof predictionMaker.getPrediction()).toEqual("object");
-      expect(predictionMaker.getPrediction()).not.toBeNull();
+    it("check if method returns a prediction object", done => {
+      predictionMaker.getPrediction().then(prediction => {
+        expect(typeof prediction).toEqual("object");
+        expect(prediction).not.toBeNull();
+        done();
+      });
     });
-    it("takes a company and returns a company property on the prediction object ", function() {
-      let companyProperty = predictionMaker.getPrediction("Apple");
-      expect(companyProperty.company).toBe("Apple");
+    it("takes a company and returns a company property on the prediction object ", done => {
+      predictionMaker.getPrediction("Apple").then(prediction => {
+        expect(prediction.company).toBe("Apple");
+        done();
+      });
     });
-    it("takes a company with multiple words and returns the company property on the prediction object ", function() {
-      let predictionObj = predictionMaker.getPrediction("Take 2 Interactive");
-      expect(predictionObj.company).toBe("Take 2 Interactive");
+    it("takes a company with multiple words and returns the company property on the prediction object ", done => {
+      predictionMaker.getPrediction("Take 2 Interactive").then(prediction => {
+        expect(prediction.company).toBe("Take 2 Interactive");
+        done();
+      });
     });
-    it("returns on object with prediction property", function() {
-      let predictionObj = predictionMaker.getPrediction("Take 2 Interactive");
-      expect(predictionObj.prediction).toBeDefined();
+    it("returns on object with prediction property", done => {
+      predictionMaker.getPrediction("Take 2 Interactive").then(prediction => {
+        expect(prediction.prediction).toBeDefined();
+        done();
+      });
     });
-    it("returns on object with prediction property of up or down", function() {
-      let predictionObj = predictionMaker.getPrediction("Take 2 Interactive");
-      expect(/^up$|^down$/.test(predictionObj.prediction)).toBe(true);
+    it("returns on object with prediction property of 1 or 0", done => {
+      predictionMaker.getPrediction("Take 2 Interactive").then(prediction => {
+        expect(prediction.prediction).toBeLessThan(2);
+        expect(prediction.prediction).toBeGreaterThan(-1);
+        expect(Number.isInteger(prediction.prediction)).toEqual(true);
+        done();
+      });
     });
-    it("returns on object with time property", function() {
-      let predictionObj = predictionMaker.getPrediction("Take 2 Interactive");
-      expect(predictionObj.time).toBeDefined();
+    it("returns on object with time property", done => {
+      predictionMaker.getPrediction("Take 2 Interactive").then(prediction => {
+        expect(prediction.time).toBeDefined();
+        done();
+      });
     });
-    it("returns on object with time of prediction creation", function() {
-      let predictionObj = predictionMaker.getPrediction("Take 2 Interactive");
-      expect(predictionObj.time).toBe(time);
+    it("returns on object with time of prediction creation", done => {
+      predictionMaker.getPrediction("Take 2 Interactive").then(prediction => {
+        expect(prediction.time).toBe(time);
+        done();
+      });
     });
-    it("returns on object with prediction time as a string", function() {
-      let predictionObj = predictionMaker.getPrediction("Take 2 Interactive");
-      expect(typeof predictionObj.time).toBe("string");
+    it("returns on object with prediction time as a string", done => {
+      predictionMaker.getPrediction("Take 2 Interactive").then(prediction => {
+        expect(typeof prediction.time).toBe("string");
+        done();
+      });
     });
-
 
     it("calls the callAlgorithim function", function() {
       this.callAlgorithim = {
         callAlgorithim: function() {}
-      }
+      };
       spyOn(this.callAlgorithim, "callAlgorithim");
       predictionMaker = new PredictionMaker(this.callAlgorithim.callAlgorithim);
       predictionMaker.getPrediction("Bananas");
       expect(this.callAlgorithim.callAlgorithim).toHaveBeenCalled();
-
-
-    })
+    });
   });
 });
 
