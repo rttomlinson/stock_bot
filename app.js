ce251a85d66a19ded5cb5d452cf1c4fcc11174a8
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-15 19:07:39 +0000

    Add predictionMaker callAlgorithim

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const TwitterWrapper = require("./lib/twitter_wrapper");
 const twit = new TwitterWrapper(require("./configtwit.json"));
 
 twit.sendPrediction("It's gonna rain!").then(response => {
-  if (typeof response === "string") {
-    console.error(response);
-  } else {
-    console.log(`Response from twitter API ${response.data}`);
-  }
+    if (typeof response === "string") {
+        console.error(response);
+    }
+    else {
+        console.log(`Response from twitter API ${response.data}`);
+    }
 });
