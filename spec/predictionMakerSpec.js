4baea6049f93c01ca0430f47019e8212f4d461e9
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-15 18:50:06 +0000

    Add callAlgorithim

--- spec/predictionMakerSpec.js
+++ spec/predictionMakerSpec.js
@@ -1,7 +1,24 @@
-const predictionMaker = require("../lib/predictionMaker");
+const {
+  predictionMaker,
+  callAlgorithim
+} = require("../lib/predictionMaker");
 
-describe("a call to prediction maker", function() {
-  it("check if method returns a string", function() {
-    expect(typeof predictionMaker()).toEqual("string");
+describe("prediction Maker", function() {
+  describe("a call to prediction maker", function() {
+    it("check if method returns a string", function() {
+      expect(typeof predictionMaker()).toEqual("string");
+    });
+    it("comes back in the form of Prediction for today at time: {name} is going to go (up/down)", function() {
+      let time = new Date(Date.now()).toDateString();
+      let prediction = `Prediction for today ${ time }: Apple stocks are going up.`;
+      console.log(prediction);
+      expect(predictionMaker('Apple')).toEqual(prediction)
+    });
+
+  });
+  describe("a call to callAlgorithim", function() {
+    it("check if method returns up or down", function() {
+      expect(/up|down/.test(callAlgorithim())).toEqual(true);
+    });
   });
 });
