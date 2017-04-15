f475999f14b4e64544b3748b7c5e011866d3cd26
William Whitworth <william10charles@gmail.com>
2017-04-15 11:13:21 -0600

    writting tests for checkLength

--- spec/twitter_wrapperSpec.js
+++ spec/twitter_wrapperSpec.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const TwitterWrapper = require('../lib/twitter_wrapper');
-
+const TwitterWrapper = require("../lib/twitter_wrapper");
 
 describe(".sendPrediction method", function() {
-    let tweet;
-    
-    beforeEach(function() {
-        tweet = new TwitterWrapper();
-    })
-    
-    it("return as a Promise", function(done) {
-        tweet.sendPrediction().then((response) => {
-            expect(true).toBe(true);
-            done();
-        });
-    });
-    
-    
-})
+  let tweet;
 
+  beforeEach(function() {
+    tweet = new TwitterWrapper();
+  });
 
+  it("return an a error if no string is passed", function() {
+    expect(tweet.checkLength()).toEqual(false);
+  });
+  it("return error if string length is greater than 140", function() {
+    let longStr = "fffasdkjfhlaksdfnalksjdfh;asdfjhlkjahsdkfjhalksjdfhlakjshdflkajshdflkjashdflkjasdfhlkajsdhflkjashdflkajshdflkjashdflkjahsdlfkjhasdlfkjhaslkdjfhalskjdfhalksjdfhlakjsdfhakjlsdhfkajsdhfkjadsfhalksdhjfasfl;kasjflaksdjf;liekjasdkl;fjakls;fdjakl;sdfjal;skdfjal;skdfjal;ksjdfal;ksdfjal;ksdfjal;skdfjal;ksdjfals;kj";
+    expect(tweet.checkLength(longStr)).toEqual(false);
+  });
+  it("expect .checkLength when called with str less than 140 chars and greater than zero to respond with 'success'", function() {
+    let goodStr = "Hello";
+    expect(tweet.checkLength(goodStr)).toEqual(true);
+  });
+});
