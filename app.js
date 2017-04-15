cead46308c509b1759cc4cb48c2e5a353a219cc3
markmarkyesyes <markhahn545@gmail.com>
2017-04-15 18:39:59 -0500

    added Set Interval and local interval tester

--- app.js
+++ app.js
@@ -6,26 +6,32 @@ const TwitterWrapper = require("./lib/twitter_wrapper");
 const twit = new TwitterWrapper(require("./configtwit.json"));
 
 ////************* Prediction Maker Module ********////
-const PredictionMaker = require('./lib/predictionMaker');
+const PredictionMaker = require("./lib/predictionMaker");
 const predictionMaker = new PredictionMaker();
 //////////*******Prediction Formatter*********//////////////////////
-const predictionFormatter = require('./lib/predictionFormatter');
+const predictionFormatter = require("./lib/predictionFormatter");
 ///////////////*****************//////////////////////
 
 //get a prediction
 async function tweetPrediction() {
-    let predictionObj = await predictionMaker.getPrediction('Apple')
-    let predictionString = predictionFormatter(predictionObj);
-    twit.sendPrediction(predictionString).then(response => {
-        if (typeof response === "string") {
-            console.error(response);
-        }
-        else {
-            console.log(`Response from twitter API ${response.data}`);
-        }
-    });
+  let predictionObj = await predictionMaker.getPrediction("Apple");
+  let predictionString = predictionFormatter(predictionObj);
+  twit.sendPrediction(predictionString).then(response => {
+    if (typeof response === "string") {
+      console.error(response);
+    } else {
+      console.log(`Response from twitter API ${response.data}`);
+    }
+  });
 }
 
-//format it
-let i = 0;
-while ()
+setInterval(tweetPrediction, 86400000);
+
+//WRITE A TEST LATER DONT BE LAZY - FOR MARK
+//////////*******Runs multiple tweets locally*********//////////////////////
+// let i = 0;
+// while (i < 2) {
+//   console.log("tweeting");
+//   setTimeout(tweetPrediction, 20000 * i);
+//   i++;
+// }
